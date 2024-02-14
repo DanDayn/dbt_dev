fb426af2a7a2fd5e4deb88b2f15a975bf9c1316c
danieldayn@gmail.com <danieldayn@gmail.com>
2024-02-14 11:00:15 +0000

    stagings set up + data validity/cleansing

---
+++ models/staging/staging_motor_vehicle.sql
@@ -0,0 +1,13 @@
+with staging_motor_vehicle as (
+    select 
+        objectid as object_id
+        , motive_power as motive_power
+        , cast(first_nz_registration_year as int) as registration_year
+        , make as vehicle_company
+        , model as vehicle_model
+    from {{ source('raw', 'motor_vehicle_register_raw') }}
+    where lower(motive_power) like '%electric%'
+        or lower(motive_power) like '%hybrid%'
+)
+
+select * from staging_motor_vehicle
