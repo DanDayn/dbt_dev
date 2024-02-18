0f97e1581e4cb0f34465e7b997d237efd11a46ff
danieldayn@gmail.com <danieldayn@gmail.com>
2024-02-18 12:57:17 +0000

    adding some extra attributes

--- models/reporting/rfact_ev_charging_stations_distances.sql
+++ models/reporting/rfact_ev_charging_stations_distances.sql
@@ -3,7 +3,7 @@ with rfact_ev_charging_stations_distances as (
     select 
         anchor_station_id
         , destination_station_id
-        , cast(distance_kms as decimal(16,2)) --Example of how rfact would be useful for analysts
+        , cast(distance_kms as decimal(16,2)) distance_kms --Example of how rfact would be useful for analysts
     from {{ref('fact_ev_charging_stations_distances')}}
 )
 
